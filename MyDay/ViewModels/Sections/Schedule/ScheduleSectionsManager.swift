e928bfda656b59fb987f21d9cc66ed6aa4104ecf
Roman Balayan <roman.balayan@openmedia.co>
2023-07-28 21:45:08 +0300

    Refactored ScheduleSectionsManager (forgot to commit)

--- MyDay/ViewModels/Sections/Schedule/ScheduleSectionsManager.swift
+++ MyDay/ViewModels/Sections/Schedule/ScheduleSectionsManager.swift
@@ -7,65 +7,12 @@
 
 import Foundation
 
-final class ScheduleSectionsManager: SectionsManager {
+final class ScheduleSectionsManager: SectionsManager<ScheduleSectionViewModel> {
     
-    override init(minDate: DateModel, maxDate: DateModel) {
-        super.init(minDate: minDate, maxDate: maxDate)
+    override init(visibleDates: [DateModel]) {
+        super.init(visibleDates: visibleDates)
         
         self.title = "Schedule"
         self.addActionTitle = "Action"
-        
-        initialize()
-    }
-    
-    init(visibleDates: [DateModel]) {
-        super.init(minDate: visibleDates.first!, maxDate: visibleDates.last!)
-        
-        self.title = "Schedule"
-        self.addActionTitle = "Action"
-        
-        initialize(visibleDates: visibleDates)
-    }
-    
-    override func initialize() {
-        //let sections = restore()
-    }
-    
-    private func initialize(visibleDates: [DateModel]) {
-        let restoredSections = restore()
-        
-        restoredSections.forEach({ key, value in
-            self.add(section: value)
-        })
-        
-        for dateModel in visibleDates {
-            if !restoredSections.keys.contains(where: { key in
-                return key == dateModel
-            }) {
-                let section = ScheduleSectionViewModel(date: dateModel)
-                section.add(ScheduleItemViewModel(title: "Test \(dateModel.month)", description: "No Desc", startDate: Date(), endDate: Date(), date: dateModel.date))
-                section.add(ScheduleItemViewModel(title: "Test \(dateModel.day)", description: "No Desc", startDate: Date(), endDate: Date(), date: dateModel.date))
-                add(section: section)
-            }
-        }
-    }
-    
-    private func restore() -> [DateModel:ScheduleSectionViewModel] {
-        let realmManager = SectionsRealmManager<ScheduleItemViewModel>()
-        let scheduleItems = realmManager.restore()
-        
-        var result: [DateModel:ScheduleSectionViewModel] = [:]
-        
-        for item in scheduleItems {
-            let dateModel = DateModel(date: item.date)
-            
-            if !result.keys.contains(where: { $0.day == dateModel.day && $0.monthInt == dateModel.monthInt }) {
-                result[dateModel] = ScheduleSectionViewModel(date: dateModel)
-            }
-            
-            result[dateModel]?.add(item)
-        }
-        
-        return result
     }
 }
