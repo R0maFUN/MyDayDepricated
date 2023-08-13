c39a36920a02886a041eb42c7dc8e78f7d5d41d9
Roman Balayan <roman.balayan@openmedia.co>
2023-08-13 11:04:18 +0300

    NotesColors

---
+++ MyDay/Resources/Colors/NotesColors.swift
@@ -0,0 +1,13 @@
+//
+//  NotesColors.swift
+//  MyDay
+//
+//  Created by Рома Балаян on 13.08.2023.
+//
+
+import Foundation
+import UIKit
+
+enum NotesColors {
+    static let primary = UIColor(named: "NotesPrimary")
+}
